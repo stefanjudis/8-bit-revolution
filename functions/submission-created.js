c43e8520cc8c1a3d1f15a0949f6c4950a2f5759d
stefan judis <stefanjudis@gmail.com>
2019-03-05 12:34:30 +0100

    Add local function development

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -9,7 +9,22 @@ const {
 } = process.env;
 const client = require('twilio')(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 
+const headers = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers':
+    'Origin, X-Requested-With, Content-Type, Accept',
+  'Access-Control-Allow-Methods': '*',
+  'Access-Control-Max-Age': '2592000',
+  'Access-Control-Allow-Credentials': 'true'
+};
+
 exports.handler = function(event, context, callback) {
+  // if (event.httpMethod === 'OPTIONS') {
+  //   console.log('options hitting!');
+  //   callback(null, { statusCode: '204', headers });
+  //   return;
+  // }
+
   Promise.all(
     CONTACT_NUMBERS.split(';').map(num => {
       return client.messages.create({
@@ -19,6 +34,6 @@ exports.handler = function(event, context, callback) {
       });
     })
   )
-    .then(() => callback())
+    .then(() => callback(null, { statusCode: 200, headers, body: 'Created' }))
     .catch(e => callback(e));
 };
